c23ba545f5687e9f8f3d7e81b0b46b79b23246bf
kimani <kimaniowens@gmail.com>
2014-09-05 10:04:14 -0500

    typos for days :l

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
 
 	validates :name, presence: true
 
-	validates :username, uniqueness: true, presence: true
+	validates :sername, uniqueness: true, presence: true
 
 	validates :email, uniqueness: true, presence: true, format: { with: /\A[\w.+-]+@([\w]+.)+\w+\z/ }
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root to: "users#new"
+  
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
